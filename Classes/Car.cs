db68b7ae2b38b93f6fcce4dd4bd83581670f2a7e
Zach Padovano <zacharyaz10@gmail.com>
2023-04-20 08:28:03 -0700

    First commit

---
+++ Classes/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+            
+        }
+        public Car(string makeInput, string modelInput, int year)
+        {
+            Make = makeInput;
+            Model = modelInput;
+            Year = year;
+        }
+        public string Make { get; set; } //get = read   set = write
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Classes
+﻿using System.Collections.Generic;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Fusion";
+            myCar.Year = 2017;
+
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 1987);
+            
+            var carList = new List<Car>();
+            carList.Add(myCar);
+            carList.Add(chevy);
+            carList.Add(mazda);
+            foreach (var car in carList)
+            {
+                Console.WriteLine(car.Make);
+                Console.WriteLine(car.Model);
+                Console.WriteLine(car.Year);
+
+                Console.WriteLine();
+            }
+
+
+
         }
     }
 }
